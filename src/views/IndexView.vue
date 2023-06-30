091f8d5831b73f9e63f459691a83d18da4cee42c
dylan <825880445@qq.com>
2023-07-01 00:33:16 +0800

    add index

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -3,8 +3,11 @@
 </script>
 
 <template>
-  <div class="w-full h-full">
-    
+  <div class="w-full h-full flex">
+    <div class="w-[60px] bg-[#111638] h-full shrink-0"></div>
+    <div class="bg-white flex-1 overflow-y-auto">
+        <RouterView class="w-full h-full" />
+    </div>
   </div>
 </template>
 
