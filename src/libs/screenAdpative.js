789daf08736b4795fd6d643b8f739f0ec2535363
dylan <825880445@qq.com>
2023-06-30 16:57:36 +0800

    适配优化

--- src/libs/screenAdpative.js
+++ src/libs/screenAdpative.js
@@ -2,9 +2,15 @@ function screenAdpative() {
   let docWidth = document.documentElement.clientWidth
   let fontsize = (docWidth / 1920) * 16
   let dpr = window.devicePixelRatio
+  // 关于dpr的处理还需要研究
+  // 外接显示器的dpr好像始终是1
+  // 笔记本的dpr是根据屏幕设置比来的，如果是125%，就是1.25
   // let customFontSize = fontsize * (1.25 / dpr)
   let customFontSize = fontsize;
-  if(customFontSize<10)customFontSize=12;
+  // 当前字体乘以当前dpr，然后除以目标dpr，可以得到和目标dpr一样的字体
+  if(dpr>1.25)customFontSize = customFontSize*dpr/1.25;
+  
+  if(customFontSize<10)customFontSize=10;
   console.log(dpr, fontsize, customFontSize)
   document.documentElement.style.fontSize = customFontSize + 'px'
 }
